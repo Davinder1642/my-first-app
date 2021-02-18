33a56e135b71980f4466635d3a696978e8f7cb5e
Davinderpal singh <Davinder.batth77@gmail.com>
2021-02-18 17:12:06 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  // I commented this before video-12th in section:1
+  // name = 'SAM';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+      // I commented this before video-12th in section:1
+        // FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
