e6a02e6cd18b6199be65912f581d2cf2295beab2
Davinderpal <davinder.batth77@gmail.com>
2021-02-22 17:10:02 -0500

    Added styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+    selector: 'app-servers' ,
+  // selector: '[app-servers]' ,
+  //   selector: '.app-servers',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+   styleUrls: ['./servers.component.css']
+
 })
 export class ServersComponent implements OnInit {
 
